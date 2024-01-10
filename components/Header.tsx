58a2f1a03ab2f1bc51096f06cf43d0ba25aab5e9
Brijesh Peshvani <brijeshpeshvani38@gmail.com>
2024-01-10 17:44:01 +0530

    Header section using react-social-icons and framer-motion

---
+++ components/Header.tsx
@@ -0,0 +1,74 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
+        {/* Social Icons */}
+        <SocialIcon
+          url="https://www.github.com/brijeshvp"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/brijesh-peshvani/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.instagram.com/brijesh.peshvani/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          fgColor="gray"
+          bgColor="transparent"
+          className="cursor-pointer"
+          network="email"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
+      </motion.div>
+    </header>
+  );
+};
+
+export default Header;
