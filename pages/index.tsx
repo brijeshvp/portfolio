c497d99b363453c2194c60ba280710ad06f443d5
Brijesh Peshvani <brijeshpeshvani38@gmail.com>
2024-01-10 20:14:16 +0530

    Hero section using react-typewriter framer-motion - done as is

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,23 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Brijesh's Portfolio</title>
       </Head>
 
-      <Header/>
-      {/* Hero */}
+      <Header />
 
+      <section id="hero" className="snap-center">
+        <Hero />
+      </section>
+      
       {/* About */}
 
       {/* Experience */}
