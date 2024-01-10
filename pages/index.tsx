99979d3dc29aae286c3a1d55cd895a4ec4344cc1
Brijesh Peshvani <brijeshpeshvani38@gmail.com>
2024-01-11 00:05:48 +0530

    Experience section done

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import WorkExperience from "@/components/WorkExperience";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +25,9 @@ export default function Home() {
         <About />
       </section>
 
-      {/* Experience */}
+      <section id="experience" className="snap-center">
+        <WorkExperience />
+      </section>
 
       {/* Skills */}
 
