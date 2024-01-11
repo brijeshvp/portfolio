574d2caed48991b4cf048a73e0f2c0a3d3f4ae5e
Brijesh Peshvani <brijeshpeshvani38@gmail.com>
2024-01-11 22:47:32 +0530

    Scrollbars and back to top button done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import Link from "next/link";
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 
@@ -40,33 +41,34 @@ const Header = (props: Props) => {
           bgColor="transparent"
         />
       </motion.div>
-
-      <motion.div
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 1.5,
-        }}
-        className="flex flex-row items-center text-gray-300 cursor-pointer"
-      >
-        <SocialIcon
-          fgColor="gray"
-          bgColor="transparent"
-          className="cursor-pointer"
-          network="email"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-          Get In Touch
-        </p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 1.5,
+          }}
+          className="flex flex-row items-center text-gray-300 cursor-pointer"
+        >
+          <SocialIcon
+            fgColor="gray"
+            bgColor="transparent"
+            className="cursor-pointer"
+            network="email"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get In Touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,13 @@ import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Brijesh's Portfolio</title>
       </Head>
@@ -44,8 +45,26 @@ export default function Home() {
         <ContactMe />
       </section>
 
-
-      {/* Contact Me */}
+      <footer className="sticky bottom-5 w-full cursor-pointer">
+        <Link href="#hero">
+          <div className="px-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="object-cover h-10 w-10 rounded-full filter grayscale text-[#F7AB0A]/80  animate-pulse"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m15 11.25-3-3m0 0-3 3m3-3v7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+          </div>
+        </Link>
+      </footer>
     </main>
   );
 }
